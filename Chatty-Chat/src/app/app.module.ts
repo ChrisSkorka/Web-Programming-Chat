e9a85bff99ed19843b7c5e33c48156e007b49873
TechSupport420 <30539703+TechSupport420@users.noreply.github.com>
2018-08-21 09:01:20 +1000

    Created grid layout of dash board

--- Chatty-Chat/src/app/app.module.ts
+++ Chatty-Chat/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule, MatProgressSpinnerModule } from '@angular/material';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -26,6 +27,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatButtonModule,
     MatProgressSpinnerModule,
     MatGridListModule,
+    MatIconModule,
     RouterModule.forRoot([
       {'path': '', 'component': LoginComponent},
       {'path': 'dash', 'component': DashboardComponent},
--- Chatty-Chat/src/app/dashboard/dashboard.component.ts
+++ Chatty-Chat/src/app/dashboard/dashboard.component.ts
@@ -7,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  channel_name:string = 'Temp';
-  username:string = '';
+  channel_name:string = 'Temp#Ch';
+  username:string = 'TechSupport420';
+  useremail:string = 'chris@email.com';
+  groups:any = [{Name:'Group 1', Participants:[1, 2, 3], GroupAdmins:[1], Channels:[0, 1, 2]}];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onGroupClick(index){
+    alert(index);
   }
 
 }
